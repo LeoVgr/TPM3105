9024ad358d39f413a99627e788ee7205ffe5db96
LeoVgr <leo.vgr@hotmail.com>
2018-11-09 17:12:23 +0100

    refactoring classe

--- katatennis/src/main/java/fr/unilim/iut/katatennis/TennisGame1.java
+++ katatennis/src/main/java/fr/unilim/iut/katatennis/TennisGame1.java
@@ -2,59 +2,39 @@ package fr.unilim.iut.katatennis;
 
 public class TennisGame1 implements TennisGame {
 
-	private int m_score1 = 0;
-	private int m_score2 = 0;
+	private int scoreJoueur1;
+	private int scoreJoueur2;
 	private String player1Name;
-	private String player2Name;
+	
 
-	public TennisGame1(String player1Name, String player2Name) {
+	public TennisGame1(String player1Name) {
 		this.player1Name = player1Name;
-		this.player2Name = player2Name;
+		
+		this.scoreJoueur1=0;
+		this.scoreJoueur2=0;
 	}
 
 	public void wonPoint(String playerName) {
-		if (playerName == "player1")
-			m_score1 += 1;
+		if (playerName == player1Name)
+			scoreJoueur1 += 1;
 		else
-			m_score2 += 1;
+			scoreJoueur2 += 1;
 	}
 
 	public String getScore() {
 		String score = "";
 		int tempScore = 0;
-		if (m_score1 == m_score2) {
-			switch (m_score1) {
-			case 0:
-				score = "Love-All";
-				break;
-			case 1:
-				score = "Fifteen-All";
-				break;
-			case 2:
-				score = "Thirty-All";
-				break;
-			default:
-				score = "Deuce";
-				break;
-
-			}
-		} else if (m_score1 >= 4 || m_score2 >= 4) {
-			int minusResult = m_score1 - m_score2;
-			if (minusResult == 1)
-				score = "Advantage player1";
-			else if (minusResult == -1)
-				score = "Advantage player2";
-			else if (minusResult >= 2)
-				score = "Win for player1";
-			else
-				score = "Win for player2";
+		if (scoreJoueur1 == scoreJoueur2) {
+			score = nameOfEgalityScored();
+		} else if (scoreJoueur1 >= 4 || scoreJoueur2 >= 4) {
+			score = scoreForAdvantageAndMore();
 		} else {
 			for (int i = 1; i < 3; i++) {
 				if (i == 1)
-					tempScore = m_score1;
+					tempScore = scoreJoueur1;
 				else {
 					score += "-";
-					tempScore = m_score2;
+					tempScore = scoreJoueur2;
 				}
 				switch (tempScore) {
 				case 0:
@@ -69,8 +49,44 @@ public class TennisGame1 implements TennisGame {
 				case 3:
 					score += "Forty";
 					break;
+				default :
+					System.err.println("Erreur");
 				}
 			}
+		}
+		return score;
+	}
+
+	private String scoreForAdvantageAndMore() {
+		String score;
+		int minusResult = scoreJoueur1 - scoreJoueur2;
+		if (minusResult == 1)
+			score = "Advantage player1";
+		else if (minusResult == -1)
+			score = "Advantage player2";
+		else if (minusResult >= 2)
+			score = "Win for player1";
+		else
+			score = "Win for player2";
+		return score;
+	}
+
+	private String nameOfEgalityScored() {
+		String score;
+		switch (scoreJoueur1) {
+		case 0:
+			score = "Love-All";
+			break;
+		case 1:
+			score = "Fifteen-All";
+			break;
+		case 2:
+			score = "Thirty-All";
+			break;
+		default:
+			score = "Deuce";
+			break;
+
 		}
 		return score;
 	}
